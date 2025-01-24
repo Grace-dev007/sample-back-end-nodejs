f581066e15f9742d10cf9c4ad456214344582cfd
Grace-dev007 <graceangeline@syngrid.com>
2025-01-24 17:28:47 +0530

    first commit

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { createUser } from "../service/user.service";
 import { ApiResponse } from "../utils/hooks/util";
+import { DocumentInstance } from "twilio/lib/rest/preview/sync/service/document";
 
 export const register = async (req: Request, res: Response) => {
     try {
@@ -24,3 +25,4 @@ export const register = async (req: Request, res: Response) => {
         });
     }
 }
+
